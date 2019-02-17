e93548c49aa2c297b349eb85578ca65a423fd68f
Syte Beimin <sbeimin@betterbe.com>
2019-02-17 21:24:10 +0100

    Create lib folder if it's missing

--- control/setup.lua
+++ control/setup.lua
@@ -1,7 +1,13 @@
 local os = require('os');
+local filesystem = require("filesystem")
 os.setenv('SERVER_LOCATION', 'https://raw.githubusercontent.com/ZyorTaelon/M.A.L.P./master/control')
 local codeURL = os.getenv('SERVER_LOCATION');
 local fileName = '/downloadCode.lua'
+
+if not filesystem.exists('/home/lib') then
+  filesystem.makeDirectory('/home/lib')
+end
+  
 local command = 'wget -f ' .. codeURL .. fileName .. ' ' .. '/home/lib' .. fileName
 print("executing command: '" .. command .. "'")
 os.execute(command);
