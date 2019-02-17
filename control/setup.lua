c7ccd221cf619d7294a986411b525c1f3d19722a
Syte Beimin <sbeimin@betterbe.com>
2019-02-17 20:48:37 +0100

    more verbose setup

--- control/setup.lua
+++ control/setup.lua
@@ -3,6 +3,8 @@ local os = require('os');
 os.setenv('SERVER_LOCATION', 'https://raw.githubusercontent.com/ZyorTaelon/M.A.L.P./master/control')
 local codeURL = os.getenv('SERVER_LOCATION');
 local fileName = '/downloadCode.lua'
-os.execute('wget -f ' .. codeURL .. fileName .. ' ' .. path .. fileName);
+local command = 'wget -f ' .. codeURL .. fileName .. ' ' .. path .. fileName
+print("executing command: '" .. command .. "'")
+os.execute(command);
 local dl = require("downloadCode");
 dl.downloadAll(path);
