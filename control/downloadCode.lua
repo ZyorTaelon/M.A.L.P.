acc7709ac47bf85cf085681a3a92b2324902137c
Syte Beimin <sbeimin@betterbe.com>
2019-02-17 21:04:08 +0100

    fix download shared

--- control/downloadCode.lua
+++ control/downloadCode.lua
@@ -1,22 +1,25 @@
 local os = require('os');
-
 local url = os.getenv('SERVER_LOCATION') .. '/';
-local filenames = {
-  'sgc.lua',
-  'shared/tunnel'
-};
+local filenames = {}
+filenames["sgc.lua"] = "sgc.lua"
+filenames["shared/tunnel.lua"] = "lib/tunnel.lua"
 
 local M = {};
 
-function M.downloadAll(location)
-  for index, name in pairs(filenames) do
-    M.download(name, location);
+function M.downloadAll()
+  for src, dest in ipairs(filenames) do
+    print("src: " .. src)
+    print("dest: " .. dest)
+    M.download(src, dest);
   end
 end
 
 -- rapid reuse may result in receiving cached pages
-function M.download(name, location)
-  os.execute('wget -f ' .. url .. name .. ' ' .. location .. '/' .. name);
+function M.download(src, dest)
+  print("downloading '" .. src .. "' to '" .. dest .. "'")
+  local dlCmd = 'wget -f ' .. url .. src .. ' /home/' .. dest;
+  print('executing ' .. dlCmd)
+  os.execute(dlCmd);
 end
 
 return M;
--- control/setup.lua
+++ control/setup.lua
@@ -1,10 +1,9 @@
-local path = '/home/lib';
 local os = require('os');
 os.setenv('SERVER_LOCATION', 'https://raw.githubusercontent.com/ZyorTaelon/M.A.L.P./master/control')
 local codeURL = os.getenv('SERVER_LOCATION');
 local fileName = '/downloadCode.lua'
-local command = 'wget -f ' .. codeURL .. fileName .. ' ' .. path .. fileName
+local command = 'wget -f ' .. codeURL .. fileName .. ' ' .. '/home/lib' .. fileName
 print("executing command: '" .. command .. "'")
 os.execute(command);
 local dl = require("downloadCode");
-dl.downloadAll(path);
+dl.downloadAll();
